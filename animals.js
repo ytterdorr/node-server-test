406bf3d52c5cd03b56898b1320f7b3d8cb07f7e1
Viktor Kronvall <viktor.kronvall@gmail.com>
2014-01-15 23:24:52 +0100

    Added router

---
+++ animals.js
@@ -0,0 +1,4 @@
+
+exports.index = function* (next) {
+  this.body = 'This will be a list of animals';
+}
--- server.js
+++ server.js
@@ -2,12 +2,17 @@ var koa = require('koa');
 var logger = require('koa-logger');
 var app = koa();
 
+var router = require('koa-router');
+
 // logger
-
 app.use(logger());
+app.use(router(app));
 
-app.use(function *(){
+
+app.get('/',function *(){
   this.body = 'Hello World this is your lord speaking.';
 });
+
+app.resource('animals', require('./animals'));
 
 app.listen(3000);
