48326317c2380b1c507b10b159e45e91cbd9e1b7
Viktor Kronvall <viktor.kronvall@gmail.com>
2014-01-16 02:24:36 +0100

    Adding client side

--- server.js
+++ server.js
@@ -2,18 +2,28 @@ var koa = require('koa');
 var logger = require('koa-logger');
 var app = koa();
 
+var send = require('koa-send');
+var serve = require('koa-static');
+
 var router = require('koa-router');
+var mount = require('koa-mount');
+
+var staticRoot = __dirname;
 
 // logger
 app.use(logger());
 app.use(router(app));
 
-
 app.get('/',function *(){
-  this.body = 'Hello World this is your lord speaking.';
+  yield send(this, 'client/views/index.html', { root: staticRoot });
 });
+
+app.use(mount('/js',serve('client/js')));
+
 
 app.resource('animals', require('./animals'));
 app.resource('games', require('./games'));
 
 app.listen(3000);
+
+console.log('Listening on port 3000');
